008d28687ec694368a8420644e525f0225c5596f
shabab qaisar <shabab.qaisar@fiveriverstech.com>
2020-03-17 18:23:20 +0500

    only /api prod

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https:/api.theclusterheadache.com/api';
+const API_ROOT = '/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
